d85882334f38908c34e736ad7130cd2ef35110a3
Imre Horvath <imi.horvath@gmail.com>
2026-04-07 18:39:22 +0200

    Improve main

--- main.c
+++ main.c
@@ -54,7 +54,7 @@ char *fileextension(char *filename)
 
 enum filetype deterfiletype(char *filename)
 {
-    int check;
+    int checks;
     char *ext = fileextension(filename);
 
     /* When file has an extension, determine file type by the extension */
@@ -66,27 +66,29 @@ enum filetype deterfiletype(char *filename)
     }
 
     /* Otherwise, try to infer type by its contents */
-    if ((check = ihexfile(filename)) == -1)
+    if ((checks = ihexfile(filename)) == -1)
         return FILETYPE_ERROR;
-    if (check)
+    if (checks)
         return FILETYPE_IHEX;
     /* TODO: More file type-checks goes here below... */
 
     return FILETYPE_UNKNOWN;
 }
 
-void usageandexit(int exitcode)
+void printusage(void)
 {
-    fprintf(stderr, "AVR Disassembler for the 8-bit AVRs. v%s (c) Imre Horvath, 2024\n", VERSION);
+    fprintf(stderr, "AVR Disassembler for the 8-bit AVRs. v%s (c) Imre Horvath, 2023\n", VERSION);
     fprintf(stderr, "Usage: %s [options] inputfile\n", command);
-    fprintf(stderr, "Currently supported inputfile types are: IHEX\n");
-    fprintf(stderr, "  IHEX: Intel hex format, file should have an extension .hex\n");
-    fprintf(stderr, "Options:\n");
-    fprintf(stderr, "  -h : Show this usage info and exit.\n");
-    fprintf(stderr, "  -l : List disabled regions, word addresses and raw instructions together with the disassembled code.\n");
-    fprintf(stderr, "  -e nnnn:nnnn : Enable disassembly of otherwise disabled region. Multiple options are possible.\n");
-    fprintf(stderr, "                 Use hex numbers. For reference, see listing of disabled regions in listing mode.\n");
-    exit(exitcode);
+
+#define USAGE_DESCRIPTION "Currently supported inputfile types are: IHEX\n" \
+"  IHEX: Intel hex format, file should have an extension .hex\n" \
+"Options:\n" \
+"  -h : Show this usage info and exit.\n" \
+"  -l : List disabled regions, word addresses and raw instructions together with the disassembled code.\n" \
+"  -e nnnn:nnnn : Enable disassembly of otherwise disabled region. Multiple options are possible.\n" \
+"                 Use hex numbers. For reference, see listing of disabled regions in listing mode.\n"
+
+    fprintf(stderr, USAGE_DESCRIPTION);
 }
 
 int main(int argc, char **argv)
@@ -99,47 +101,80 @@ int main(int argc, char **argv)
 
     command = cmdname(argv[0]);
 
-    if (argc < 2)
-        usageandexit(1);
+    if (argc < 2) {
+        printusage();
+        return 1;   /* Error */
+    }
 
     enaregs = allocregions();
-    if (!enaregs)
-        fprintf(stderr, "Error allocating memory\n"), exit(1);
+    if (!enaregs) {
+        fprintf(stderr, "Error allocating memory\n");
+        return 1;   /* Error */
+    }
 
-    for (i = 1; i < argc; i++)
-        if (*argv[i] == '-')
+    for (i = 1; i < argc; i++) {
+        if (*argv[i] == '-') {
+            /* Process options */
             if (!strcmp(argv[i], "-l"))
                 listing = 1;
-            else if (!strcmp(argv[i], "-h"))
-                usageandexit(0);
-            else if (!strcmp(argv[i], "-e")) {
-                if (i+1 >= argc)
-                    fprintf(stderr, "Address after option -e missing.\n"), usageandexit(1);
+            else if (!strcmp(argv[i], "-h")) {
+                freeregions(enaregs);
+                printusage();
+                return 0;   /* Success */
+            } else if (!strcmp(argv[i], "-e")) {
+                if (i+1 >= argc) {
+                    freeregions(enaregs);
+                    fprintf(stderr, "Address after option -e missing.\n");
+                    return 1;   /* Error */
+                }
                 i++;
-                if (sscanf(argv[i], "%x:%x", &begin, &end) != 2)
-                    fprintf(stderr, "Option -e : Failed to parse a hex memory address range.\n"), usageandexit(1);
-                if (begin > end)
-                    fprintf(stderr, "Option -e : Starting address must be smaller or equal than end address.\n"), exit(1);
-                if (!addregion(enaregs, begin, end))
-                    fprintf(stderr, "Error allocating memory\n"), exit(1);
-            } else
-                fprintf(stderr, "Invalid option %s\n", argv[i]), usageandexit(1);
-        else
-            if (filename)
-                fprintf(stderr, "%s expects a single filename\n", command), usageandexit(1);
-            else
+                if (sscanf(argv[i], "%x:%x", &begin, &end) != 2) {
+                    freeregions(enaregs);
+                    fprintf(stderr, "Option -e : Failed to parse a hex memory address range.\n");
+                    return 1;   /* Error */
+                }
+                if (begin > end) {
+                    freeregions(enaregs);
+                    fprintf(stderr, "Option -e : Starting address must be smaller or equal than end address.\n");
+                    return 1;   /* Error */
+                }
+                if (!addregion(enaregs, begin, end)) {
+                    freeregions(enaregs);
+                    fprintf(stderr, "Error allocating memory\n");
+                    return 1;   /* Error */
+                }
+            } else {
+                freeregions(enaregs);
+                fprintf(stderr, "Invalid option %s\n", argv[i]);
+                return 1;   /* Error */
+            }
+        } else {
+            /* Process arguments */
+            if (!filename)
                 filename = argv[i];
+            else {
+                freeregions(enaregs);
+                fprintf(stderr, "%s expects a single filename\n", command);
+                return 1;   /* Error */
+            }
+        }
+    }
 
-    if (!filename)
-        fprintf(stderr, "No filename specified\n"), usageandexit(1);
+    if (!filename) {
+        freeregions(enaregs);
+        fprintf(stderr, "No filename specified\n");
+        return 1;   /* Error */
+    }
 
     switch (deterfiletype(filename)) {
         case FILETYPE_ERROR:
+            freeregions(enaregs);
             fprintf(stderr, "Error occured during determining file type %s\n", filename);
-            return 1;
+            return 1;   /* Error */
         case FILETYPE_UNKNOWN:
+            freeregions(enaregs);
             fprintf(stderr, "Unknown file type %s\n", filename);
-            return 1;
+            return 1;   /* Error */
         case FILETYPE_IHEX:
             wl = parseihexfile(filename);
             break;
@@ -151,5 +186,5 @@ int main(int argc, char **argv)
     freewordlist(wl);
     freeregions(enaregs);
 
-    return 0;
+    return 0;   /* Success */
 }
